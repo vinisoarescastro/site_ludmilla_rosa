b14a24f7b9218e45acdef5327f19cf136d1966de
Vinicius Soares <vinisoarescastro@gmail.com>
2024-10-04 09:47:54 -0300

    modal z-index comunicado/aviso

---
+++ js/modal.js
@@ -0,0 +1,10 @@
+
+// Função para abrir o modal automaticamente ao carregar a página
+window.onload = function() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+// Função para fechar o modal
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
